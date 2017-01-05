d604cefc7c381e602cda8985aab5a34dbb1b1017
wuzhuo <me19@qq.com>
2017-01-05 19:09:57 +0800

    增加 关键词和描述信息

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@section('title')微信小程序商店@show - WX小程序 - Powered by WxStore</title>
+    <meta name="keywords" content="@section('keywords') @show小程序, 微信小程序, wewx, WX小程序, 开发, 教程, 推广, 微信应用号, 微信, 商店, 市场, 小程序商店, 小程序大全, 小程序发布站" />
+    <meta name="author" content="WxStore" />
+    <meta name="description" content="@section('description') WX小程序，微信小程序商店。您可以第一时间体验到最新最好的小程序。您可以在这里发布您的小程序，让你的小程序无人不知。 @show" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ cdn(elixir('css/app.css')) }}">
