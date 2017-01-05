4d99f684a4d879a4beb76e2a7567b896063a1a89
wuzhuo <me19@qq.com>
2017-01-06 00:49:03 +0800

    update  进一步调整登录注册页面

--- app/Repositories/WxappRepositoryEloquent.php
+++ app/Repositories/WxappRepositoryEloquent.php
@@ -45,7 +45,7 @@ class WxappRepositoryEloquent extends BaseRepository implements WxappRepository
 
     public function getFromeCache()
     {
-        return $this->model->with('tags')->paginate(12);
+        return $this->model->paginate(12);
     }
 
 }
