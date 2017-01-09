7ffe34f175a89d01e2939b722aaaf045dda79187
luffy <luffy@comicool.cn>
2017-01-09 22:44:52 +0800

    增加百度统计

--- resources/views/layouts/partials/script.blade.php
+++ resources/views/layouts/partials/script.blade.php
@@ -7,18 +7,13 @@
 <!-- 统计代码 -->
 <div style="display: none"><script src="{{ cdn(elixir('js/19083481.js')) }}"></script></div>
 
-<!-- 百度Push-->
+<!-- 百度-->
 <script>
-    (function(){
-        var bp = document.createElement('script');
-        var curProtocol = window.location.protocol.split(':')[0];
-        if (curProtocol === 'https') {
-            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-        }
-        else {
-            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-        }
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?69cd8117fa4ac3c4f4bdb93cf6aa55be";
         var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(bp, s);
+        s.parentNode.insertBefore(hm, s);
     })();
 </script>
